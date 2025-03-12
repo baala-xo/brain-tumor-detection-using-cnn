d357525bae4ac4b359db44472898c66f58741a6c
Bala <102295527+baala-xo@users.noreply.github.com>
2025-03-12 19:46:22 +0530

    updated port

--- app.py
+++ app.py
@@ -116,4 +116,4 @@ iface = gr.Interface(
     description="Upload a brain MRI scan to detect tumors, view the heatmap."
 )
 
-iface.launch(server_name="0.0.0.0", server_port=port)
+iface.launch(server_name="0.0.0.0", server_port=int(os.environ.get("PORT", 7860)))
